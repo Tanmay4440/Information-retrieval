dc07dd492c9176940e93c7409e00f347ec1a56dd
TanmayGaurav <196471103+Tanmay4440@users.noreply.github.com>
2026-06-14 01:38:25 +0530

    Initial commit

---
+++ src/helper.py
@@ -0,0 +1,76 @@
+import os
+from PyPDF2 import PdfReader
+from dotenv import load_dotenv
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_huggingface import HuggingFaceEmbeddings
+
+load_dotenv()
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+
+        for page in pdf_reader.pages:
+            page_text = page.extract_text()
+            if page_text:
+                text += page_text
+
+    return text
+
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+
+    chunks = text_splitter.split_text(text)
+
+    return chunks
+
+
+def get_vector_store(text_chunks):
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    vector_store = FAISS.from_texts(
+        texts=text_chunks,
+        embedding=embeddings
+    )
+
+    return vector_store
+
+
+def get_conversational_chain(vector_store):
+
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-1.5-flash",
+        google_api_key=GOOGLE_API_KEY,
+        temperature=0.3
+    )
+
+    memory = ConversationBufferMemory(
+        memory_key="chat_history",
+        return_messages=True
+    )
+
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vector_store.as_retriever(),
+        memory=memory
+    )
+
+    return conversation_chain
